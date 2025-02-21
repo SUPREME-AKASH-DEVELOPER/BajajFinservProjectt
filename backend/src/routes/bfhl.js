c6f63f5b8883aa98c854f062923efd55f4ce2963
Akash Lakhwan <142366831+SUPREME-AKASH-DEVELOPER@users.noreply.github.com>
2025-02-21 19:54:00 +0530

    Delete backend/src directory

--- backend/src/routes/bfhl.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getOperationCode, processArray } from '../controllers/bfhl.js';
-
-const router = Router();
-
-router.get('/', getOperationCode);
-router.post('/', processArray);
-
-export { router };
